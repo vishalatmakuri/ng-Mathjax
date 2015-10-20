68249fc0557c6b8d1b71774b24664380d0856b21
vishalatmakuri <findvishal@gmail.com>
2015-10-20 14:54:25 -0500

    added error handle

--- ng-Mathjax.js
+++ ng-Mathjax.js
@@ -1,24 +1,32 @@
-angular.module('mathJaxRender',[])
-.directive('ngMathjax', function () {
+angular.module('mathJaxRender', [])
+    .directive('ngMathjax', function () {
 
-MathJax.Hub.Config({
-  skipStartupTypeset: true
-});
-MathJax.Hub.Configured();
-        return{
-        restrict:'A',
-        scope:{
-            render:'='
-        },
-        tamplate:'<div><div>',
-        link: function ($scope, $element, $attrs){
-
-            $scope.$watch(   'render' ,function(formula){
-                var parsed = math.parse(formula) ;
-                var latex = parsed.value !=='undefined'? parsed.toTex({parenthesis:'keep'}):'';
-                $element.html('$$'+latex+'$$')
-                MathJax.Hub.Queue(['Typeset',  MathJax.Hub, $element[0]]);
-            })
-        }
-    };
-});
+        MathJax.Hub.Config({
+            skipStartupTypeset: true
+        });
+        MathJax.Hub.Configured();
+        return {
+            restrict: 'A',
+            scope: {
+                render: '='
+            },
+            link: function ($scope, $element, $attrs) {
+                $scope.$watch('render', function (formula) {
+                    try{
+                        var parsed = math.parse(formula);
+                    } catch (e){
+                        $element[0].style['background-color']='#F8C7C7';
+                    }
+                    if(parsed){
+                        $element[0].style['background-color'] = 'white';
+                        var latex = parsed.value !== 'undefined' ? parsed.toTex({
+                            parenthesis: 'keep'
+                        }) : '';
+                        $scope.style='';
+                        $element.html('$$' + latex + '$$');
+                        MathJax.Hub.Queue(['Typeset', MathJax.Hub, $element[0]]);
+                    }
+                });
+            }
+        };
+    });
